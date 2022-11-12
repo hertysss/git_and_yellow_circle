bb52a4732b41a702cb4f412de83c9eea264adfb4
User <user@gmail.com>
2022-11-12 13:04:39 +0300

    интерфейс подключается к программе не в виде ui-файла, а в виде отдельного класса на языке программирования Python

---
+++ UI.py
@@ -0,0 +1,18 @@
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(400, 300)
+        self.btn = QtWidgets.QPushButton(Form)
+        self.btn.setGeometry(QtCore.QRect(160, 20, 75, 23))
+        self.btn.setObjectName("btn")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.btn.setText(_translate("Form", " ЖМИ"))
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import sys
 from random import randint
 
 from PyQt5 import uic, QtGui
+from UI import Ui_Form
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
-class MyWidget(QMainWindow):
+class MyWidget(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.btn.clicked.connect(self.paint)
 
     def paint(self):
@@ -23,11 +24,10 @@ class MyWidget(QMainWindow):
             qp.end()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(randint(0, 255), randint(0, 255), randint(0, 255)))
         radius = randint(10, 200)
         x, y = randint(10, 200), randint(10, 200)
         qp.drawEllipse(x, y, radius, radius)
-
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
